05df414104deecb0d30d4b2875f8daeeb8a439e2
Ronald Ferdinand <mail.ronaldferdinand@gmail.com>
2023-07-07 21:02:44 +0700

    v2.0

--- src/views/divisions/CreateDivision.vue
+++ src/views/divisions/CreateDivision.vue
@@ -1,9 +1,97 @@
 <template>
-    <h1>CREATE</h1>
+    <div class="container my-2">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4 class="m-4">Form Input Divisi</h4>
+                        <hr>
+                        <form @submit.prevent="submitForm">
+                            <div style="margin-left: 4rem;">
+                                <div v-if="errors">
+                                    <strong style="color: red;">{{ errors }}</strong>
+                                </div>
+                                <div class="form-group d-flex align-items-center justify-content-start my-4">
+                                    <label class="col-2" for="name"><strong style="color: red;">*</strong> Nama</label>
+                                    <input class="form-control" style="width: 70% !important" type="text" id="name" v-model="division.name" required>
+                                </div>
+                                <div class="form-group d-flex align-items-center justify-content-start my-4">
+                                    <label class="col-2" for="code"><strong style="color: red;">*</strong> Deskripsi</label>
+                                    <input class="form-control" style="width: 70% !important" type="text" id="code" v-model="division.description" required>
+                                </div>
+                                <div class="form-group d-flex align-items-center justify-content-start my-4">
+                                    <label class="col-2" for="position">Status</label>
+                                    <select class="form-control" style="width: 70% !important" id="position" v-model="division.status" required>
+                                        <option value="1">Aktif</option>
+                                        <option value="2">Tidak Aktif</option>
+                                    </select>
+                                </div>                                
+                                <span style="font-size: small;">Kolom dengan tanda <strong style="color: red; font-size: medium;">*</strong> berarti<strong> Wajib diisi.</strong></span>
+                            </div>
+                            <hr>
+                            <div class="d-flex align-items-center justify-content-end m-4">
+                                <button type="submit" class="btn btn-md btn-success shadow">SIMPAN</button>
+                                <router-link :to="{name: 'divisions.index'}" class="btn btn-md btn-light border shadow mx-4">
+                                    KEMBALI
+                                </router-link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-
+  
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            division: {
+                name: '',
+                description: '',
+                status: '1',
+            },
+            errors: null,
+        };
+    },
+    methods: {
+        submitForm() {
+            const formData = new FormData();
+            formData.append('name', this.division.name);
+            formData.append('description', this.division.description);
+            formData.append('status', this.division.status);
+            
+            axios.post('http://127.0.0.1:8000/api/division/create', formData)
+                .then(response => {
+                    if(response.data.original.code == '200'){
+                        this.division = {
+                            name: '',
+                            description: '',
+                            status: '',
+                        };
+                        this.$router.push({ name: 'divisions.index' });
+                    } else {
+                        this.errors = response.data.original.message;
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }
+};
+</script>  
+
+<style>
+.form-group {
+  margin-bottom: 20px;
 }
-</script>
+
+label{
+    font-size: large;
+    font-weight: 600;
+}
+</style>
