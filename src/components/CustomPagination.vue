1f4ad20e7a284ea207fc069bd196b3e76fd37e02
Ronald Ferdinand <mail.ronaldferdinand@gmail.com>
2023-07-07 13:19:58 +0700

    v1.4

---
+++ src/components/CustomPagination.vue
@@ -0,0 +1,90 @@
+<template>
+    <ul class="pagination d-flex justify-content-between align-items-center">
+        <li :class="{ 'disabled': currentPage === 1 }">
+            <a @click="goToPage(currentPage - 1)" class="page-link btn-nav">Previous</a>
+        </li>
+        <div class="number">
+            <li v-for="page in totalPages" :key="page" :class="{ 'active': page === currentPage }">
+                <a @click="goToPage(page)" class="page-link">{{ page }}</a>
+            </li>
+        </div>
+        <li :class="{ 'disabled': currentPage === totalPages }">
+            <a @click="goToPage(currentPage + 1)" class="page-link btn-nav">Next</a>
+        </li>
+    </ul>
+</template>
+  
+<script>
+    export default {
+        props: {
+            currentPage: {
+                type: Number,
+                required: true
+            },
+            totalPages: {
+                type: Number,
+                required: true
+            }
+        },
+        methods: {
+            goToPage(page) {
+                console.log(page, this.currentPage)
+                if (page >= 1 && page <= this.totalPages) {
+                    this.$emit('page-changed', page);
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    .pagination {
+        display: inline-block;
+        padding-left: 0;
+        margin: 20px 4rem;
+        border-radius: 4px;
+    }
+  
+    .pagination li {
+        display: inline;
+    }
+
+    .pagination li a:hover {
+        display: inline;
+        background-color: #198754;
+        border-color: #198754;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    .pagination .number li a {
+        margin: 0 0.5rem;
+    }
+  
+    .pagination li a {
+        background-color: #fff;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        color: black;
+        float: left;
+        line-height: 1.42857143;
+        margin-left: -1px;
+        padding: 6px 12px;
+        position: relative;
+        text-decoration: none;
+    }
+  
+    .pagination li.disabled a {
+        color: #777;
+        cursor: not-allowed;
+    }
+  
+    .pagination .number li.active a {
+        z-index: 2;
+        color: #fff;
+        background-color: #198754;
+        border-color: #198754;
+        cursor: default;
+    }
+</style>
+  
--- src/views/employees/IndexEmployee.vue
+++ src/views/employees/IndexEmployee.vue
@@ -4,11 +4,31 @@
             <div class="col-md-12">
                 <div class="card border-0 rounded shadow">
                     <div class="card-body">
-                        <h4>DATA KARYAWAN</h4>
+                        <h4 class="m-4">DATA KARYAWAN</h4>
                         <hr>
-                        <router-link :to="{name: 'employees.create'}" class="btn btn-md btn-success float-end mb-3">
-                            TAMBAH KARYAWAN
-                        </router-link>
+                        <div class="d-flex justify-content-between align-items-center m-4">
+                            <div class="input-search">
+                                <svg-icon type="mdi" class="icon" :path="mdi_search"></svg-icon>
+                                <input v-model="searchQuery" placeholder="Cari karyawan">
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <label for="division" style="margin-right: 1rem;">Divisi :</label>
+                                <select v-model="searchDivision" class="select-search" id="division">
+                                    <option value="">Semua Divisi</option>
+                                    <option v-for="division in divisions" :key="division.name" :value="division.name">{{ division.name }}</option>
+                                </select>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <label for="position" style="margin-right: 1rem;">Posisi :</label>
+                                <select v-model="searchPosition" class="select-search" id="position">
+                                    <option value="">Semua Posisi</option>
+                                    <option v-for="position in positions" :key="position.name" :value="position.name">{{ position.name }}</option>
+                                </select>
+                            </div>
+                            <router-link :to="{name: 'employees.create'}" class="btn btn-md btn-success">
+                                TAMBAH KARYAWAN
+                            </router-link>
+                        </div>
 
                         <table class="table table-striped table-bordered mt-4">
                             <thead class="thead-dark">
@@ -23,7 +43,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="(employee, index) in employees" :key="index">
+                                <tr v-for="employee in paginatedData" :key="employee.id">
                                     <td>{{ employee.name }}</td>
                                     <td class="text-center">{{ employee.code }}</td>
                                     <td>{{ employee.position.name }}</td>
@@ -44,6 +64,7 @@
                                 </tr>
                             </tbody>
                         </table>
+                        <CustomPagination :current-page="currentPage" :total-pages="totalPages" @page-changed="handlePageChanged" />
                     </div>
                 </div>
             </div>
@@ -55,11 +76,13 @@
 import axios from 'axios'
 import SvgIcon from '@jamescoyle/vue-icon'
 import { onMounted, ref } from 'vue'
-import { mdiPencil, mdiEye, mdiDelete } from '@mdi/js'
+import { mdiPencil, mdiEye, mdiDelete, mdiMagnify } from '@mdi/js'
+import CustomPagination from '../../components/CustomPagination.vue';
 
 export default {
     components: {
-        SvgIcon
+        SvgIcon,
+        CustomPagination
     },
     setup() {
         let employees = ref([])
@@ -67,8 +90,11 @@ export default {
         onMounted(() => {
             axios.get('http://localhost:8000/api/employees')
                 .then(response => {
-                console.log(response.data.original.data)
                     employees.value = response.data.original.data
+                    const code      = response.data.original.code
+                    const message   = response.data.original.message
+                    const res       = {'code': code, 'message': message}
+                    console.log(res)
                 })
                 .catch(error => {
                     console.log(error.response.data)
@@ -77,7 +103,11 @@ export default {
 
         function deleteEmployee(id) {
             axios.post(`http://localhost:8000/api/employee/delete/${id}`)
-                .then(() => {
+                .then(response => {
+                    const code      = response.data.original.code
+                    const message   = response.data.original.message
+                    const res       = {'code': code, 'message': message}
+                    console.log(res)
                 }).catch(error => {
                     console.log(error.response.data);
                 })
@@ -90,10 +120,74 @@ export default {
     },
     data() {
         return {
-            mdi_eye: mdiEye,
-            mdi_pencil: mdiPencil,
-            mdi_delete: mdiDelete,
+            mdi_eye        : mdiEye,
+            mdi_pencil     : mdiPencil,
+            mdi_delete     : mdiDelete,
+            mdi_search     : mdiMagnify,
+            perPage        : 10,
+            currentPage    : 1,
+            searchQuery    : '',
+            searchDivision : '',
+            searchPosition : '',
+            divisions      : [],
+            positions      : []
         }
+    },
+    computed: {
+        totalPages() {
+            return Math.ceil(this.employees.length / this.perPage);
+        },
+        paginatedData() {
+            const start = (this.currentPage - 1) * this.perPage;
+            const end   = start + this.perPage;
+            return this.filteredData.slice(start, end);
+        },
+        filteredData() {
+            let filtered = this.employees
+            if(this.employees.length != 0){
+                filtered = this.employees.filter(employee => {
+                    const employeeName     = (employee.name ?? '').toLowerCase();
+                    const divisionName     = (employee.division.name ?? '').toLowerCase();
+                    const positionName     = (employee.position.name ?? '').toLowerCase();
+                    const query            = this.searchQuery.toLowerCase();
+                    const selectedDivision = this.searchDivision ? this.searchDivision.toLowerCase() : '';
+                    const selectedPosition = this.searchPosition ? this.searchPosition.toLowerCase() : '';
+                    return (
+                        (employeeName.includes(query) || divisionName.includes(query) || positionName.includes(query)) &&
+                        (selectedDivision === '' || divisionName === selectedDivision) &&
+                        (selectedPosition === '' || positionName === selectedPosition)
+                    );
+                });
+            }
+            return filtered;
+        },
+    },
+    mounted() {
+        this.fetchDivisions();
+        this.fetchPositions();    
+    },
+    methods: {
+        handlePageChanged(currentPage) {
+            this.currentPage = currentPage;
+        },
+        fetchDivisions() {
+            axios.get('http://127.0.0.1:8000/api/divisions')
+                .then(response => {
+                    this.divisions = response.data.original.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        fetchPositions() {
+            axios.get('http://127.0.0.1:8000/api/positions')
+                .then(response => {
+                    this.positions = response.data.original.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
     }
 }
 </script>
@@ -112,5 +206,28 @@ export default {
 
     .icon{
         color: black;
+    }
+
+    .input-search{
+        border: 1px solid black;
+        border-radius: 8px;
+        padding: 0.3rem 0.5rem;
+    }
+
+    .input-search input{
+        border: none !important;
+        margin-left: 0.5rem;
+    }
+
+    .input-search input:focus{
+        border: none !important;
+        outline: none !important;
+    }
+
+    .select-search{
+        border: 1px solid black;
+        border-radius: 8px;
+        width: 12rem;
+        padding: 0.5rem 0.5rem;
     }
 </style>
